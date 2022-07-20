fe659dee8952422a22e932559116e9b8e8394c39
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-07-19 18:58:02 -0700

    feat: package init, mock data, themes and goals outlined

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # WorldBoss-API
+
+# What is this? 
+<!-- Awareness of NbS(Nature based solutions) is... crucial! -->
+The World Boss API is a source for people who want to take action and invest in our planet and a sustainable future.
+
+# Why? 
+"We are determined to take the bold and transformative steps which are urgently needed to shift the world onto a sustainable and resilient path."
+
+
+## Three Dimensions of Sustainable Development
+   1. Economic
+   2. Social
+   3. Environmental
+## Sources
+https://sdgs.un.org/2030agenda 
---
+++ data.js
@@ -0,0 +1,63 @@
+// Mock Activity 1
+export const activities = [
+  {
+    activity: "Compost your organic waste",
+    theme: "Biodiversity",
+    availability: 0.25,
+    participants: 2,
+    region: "Asia",
+    accessability: "Few to no challenges",
+    duration: "minutes",
+    kidFriendly: true,
+    link: "#",
+  },
+];
+// Mock Activity 2
+export const activities2 = [
+  {
+    activity: "Compost your organic waste",
+    accessability: "Few to no challenges",
+    availability: 0.25,
+    duration: "minutes",
+    goal: 1-17,
+    kidFriendly: true,
+    link: "#",
+    participants: 2,
+    region: "Asia",
+    theme: "Biodiversity",
+  },
+];
+
+// IUCN Themes
+export const themes = [
+  `Biodiversity`,
+  `Business, finance and economics`,
+  `Climate change`,
+  `Freshwater and water security`,
+  `Governance, law and rights`,
+  `Nature-based Solutions`,
+  `Ocean and coasts`,
+  `Protected areas and land use`,
+  `Fight the new drug`
+];
+
+// UN Sustainable Development Goals
+export const goals = [
+  `No Poverty`,
+  `Zero Hunger`,
+  `Good Health and Well-being`,
+  `Quality Education`,
+  `Gender Equality`,
+  `Clean Water and Sanitation`,
+  `Affordable and Clean Energy`,
+  `Decent Work and Economic Growth`, 
+  `Industry, Innovation and Infastructure`,
+  `Reduce Inequalities`,
+  `Sustainable Cities and Communities`,
+  `Responsible Consumption and Production`,
+  `Climate Action`,
+  `Life Below Water`,
+  `Life on Land`,
+  `Peace, Justice and Strong Institutions`,
+  `Partnerships`,
+]
