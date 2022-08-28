3479ddd02c4bab2fbc813a7eb5f3fdc81cfb4f9b
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-08-27 23:48:56 -0700

    refactor: removed comments from knexfile.js "

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,33 +1,3 @@
-// require("dotenv").config();
-
-// const pg = require("pg");
-
-// if (process.env.DATABASE_URL) {
-//   pg.defaults.ssl = { rejectUnauthorized: false };
-// }
-
-// const sharedConfig = {
-//   client: "pg",
-//   migrations: { directory: "../api/data/migrations" },
-//   seeds: { directory: "../api/data/seeds" },
-// };
-
-// module.exports = {
-//   development: {
-//     ...sharedConfig,
-//     connection: process.env.DEV_DATABASE_URL,
-//   },
-//   testing: {
-//     ...sharedConfig,
-//     connection: process.env.TESTING_DATABASE_URL,
-//   },
-//   production: {
-//     ...sharedConfig,
-//     connection: process.env.DATABASE_URL,
-//     pool: { min: 2, max: 10 },
-//   },
-// };
-
 require("dotenv").config({ path: "../.env" });
 
 const pg = require("pg");
